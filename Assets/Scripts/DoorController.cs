75b101b677b50bd8a6e52cd8c28268daa64d639d
Avvind <oyvinden@hotmail.com>
2025-03-18 17:58:28 +0100

    More DoorController experimenting

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -2,27 +2,17 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    public Transform hinge; // Rotasjonspunktet for døren
+    public Transform doorHinge; // Hinge (DoorHinge) transform
     private bool isGrabbed = false; // Sjekker om spilleren kontrollerer døren
-    private Quaternion closedRotation; // Startrotasjonen til døren
-    private float closeThreshold = 10f; // Grense for automatisk lukking
+    public float closeThreshold = 20f; // Grense for automatisk lukking
     public float rotationSpeed = 5f; // Hvor raskt døren roteres med musa
-    [SerializeField] private Rigidbody _rigidbody;
-
-    //Read only properties
-    public Rigidbody Rigidbody => _rigidbody;
-
-    private void Start()
-    {
-        closedRotation = Quaternion.Euler(0, 0, 0); // Lukket posisjon = 0 grader
-    }
+    public Transform doorCubeMesh; // Referanse til selve dør-meshen (DoorCubeMesh)
 
     private void Update()
     {
-        // Trykk F for å toggle dørkontroll
         if (Input.GetKeyDown(KeyCode.F))
         {
-            isGrabbed = !isGrabbed; // Bytter mellom å kontrollere eller ikke
+            isGrabbed = !isGrabbed; // Toggle kontroll
         }
 
         if (isGrabbed)
@@ -38,14 +28,14 @@ public class DoorController : MonoBehaviour
     private void HandleDoor()
     {
         float mouseX = Input.GetAxis("Mouse X"); // Henter musebevegelse horisontalt
-        Vector3 newRotation = hinge.localEulerAngles;
-        newRotation.y += mouseX * rotationSpeed; // Roterer kun rundt Y-aksen
-        hinge.localEulerAngles = newRotation;
+        Vector3 newRotation = doorHinge.localEulerAngles; // Bruker Hinge-pivot for rotasjon
+        newRotation.y += mouseX * rotationSpeed; // Roterer døren på Y-aksen
+        doorHinge.localEulerAngles = newRotation;
     }
 
     private void CheckClose()
     {
-        float angle = Mathf.Abs(Mathf.DeltaAngle(hinge.localEulerAngles.y, 0)); // Beregner reell vinkel til 0 (lukket)
+        float angle = Mathf.Abs(Mathf.DeltaAngle(doorCubeMesh.localEulerAngles.y, 0)); // Beregner reell vinkel til lukket posisjon (0)
         if (angle < closeThreshold) // Hvis døren er nesten lukket
         {
             Close(); // Lukk døren
@@ -54,6 +44,6 @@ public class DoorController : MonoBehaviour
 
     private void Close()
     {
-        hinge.rotation = closedRotation; // Setter døren tilbake til startposisjon
+        doorCubeMesh.localEulerAngles = new Vector3(0, 0, 0); // Sett døren tilbake til lukket posisjon
     }
 }
